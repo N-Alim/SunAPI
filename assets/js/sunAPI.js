c5325c50a249cc16590d67bef81c7379d6bc4ec4
Nouira Alim <nouiraalim@gmail.com>
2022-01-27 17:02:21 +0100

    Mise à jour des données affiichées de l'API

--- assets/js/sunAPI.js
+++ assets/js/sunAPI.js
@@ -16,7 +16,7 @@ function loadData(latitude, longitude)
 		if (this.status == 200)
 		{
 			data = JSON.parse(this.responseText).results;
-			document.querySelector(".info").innerText = data.day_length;
+			createDataCard(data);
 			console.log(data);
 		}
 		else
@@ -30,32 +30,63 @@ function loadData(latitude, longitude)
 	xhttp.send();
 }
 
+function createDataCard(data)
+{
+	const card = document.createElement("div");
+	card.className = "dataCard";
+
+
+	for (const property in data) 
+	{
+		const textZone = document.createElement("p");
+		textZone.innerText = `${property}: ${data[property]}`;
+		card.appendChild(textZone);
+	}
+
+	document.querySelector(".info").appendChild(card);
+}
+
 // loadDoc(36.7201600, -4.4203400)
 
 //4 chiffres après la virgule suffisent pour les coordonées
 
-// loadDoc("url-1", myFunction1);
-//
-// loadDoc("url-2", myFunction2);
-//
-// function loadDoc(url, cFunction) {
-//   const xhttp = new XMLHttpRequest();
-//   xhttp.onload = function() {cFunction(this);}
-//   xhttp.open("GET", url);
-//   xhttp.send();
-// }
-//
-// function myFunction1(xhttp) {
-//   // action goes here
-// }
-// function myFunction2(xhttp) {
-//   // action goes here
-// }
+/*
+loadDoc("url-1", myFunction1);
 
+loadDoc("url-2", myFunction2);
 
+function loadDoc(url, cFunction) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {cFunction(this);}
+  xhttp.open("GET", url);
+  xhttp.send();
+}
 
-// lat (float): Latitude in decimal degrees. Required.
-// lng (float): Longitude in decimal degrees. Required.
-// date (string): Date in YYYY-MM-DD format. Also accepts other date formats and even relative date formats. If not present, date defaults to current date. Optional.
-// callback (string): Callback function name for JSONP response. Optional.
-// formatted (integer): 0 or 1 (1 is default). Time values in response will be expressed following ISO 8601 and day_length will be expressed in seconds. Optional.
+function myFunction1(xhttp) {
+  // action goes here
+}
+function myFunction2(xhttp) {
+  // action goes here
+}
+*/
+
+/*
+astronomical_twilight_begin: "3:52:31 AM"
+astronomical_twilight_end: "5:55:32 PM"
+civil_twilight_begin: "4:43:38 AM"
+civil_twilight_end: "5:04:25 PM"
+day_length: "11:38:45"
+nautical_twilight_begin: "4:18:01 AM"
+nautical_twilight_end: "5:30:02 PM"
+solar_noon: "10:54:01 AM"
+sunrise: "5:04:39 AM"
+sunset: "4:43:24 PM"
+*/
+
+/*
+lat (float): Latitude in decimal degrees. Required.
+lng (float): Longitude in decimal degrees. Required.
+date (string): Date in YYYY-MM-DD format. Also accepts other date formats and even relative date formats. If not present, date defaults to current date. Optional.
+callback (string): Callback function name for JSONP response. Optional.
+formatted (integer): 0 or 1 (1 is default). Time values in response will be expressed following ISO 8601 and day_length will be expressed in seconds. Optional.
+*/
